542739602908dabac8643c9f3130f2fe8723189d
veprogames <75524847+veprogames@users.noreply.github.com>
2025-01-31 19:10:22 +0100

    firepower DPS editor tool

---
+++ src/player/player_firepower_level_tool.gd
@@ -0,0 +1,22 @@
+@tool
+extends Node2D
+
+func _ready() -> void:
+	visibility_changed.connect(refresh)
+
+func refresh() -> void:
+	if visible and Engine.is_editor_hint():
+		var calc_dps: float = 0.0
+		var index: int = get_parent().get_children().find(self)
+		for child: Node in get_children():
+			var pod: BulletPodPlayer = child as BulletPodPlayer
+			if pod:
+				var bullet: Bullet = pod.bullet.instantiate() as Bullet
+				calc_dps += bullet.damage / pod.interval
+				bullet.queue_free()
+		var desired: float = get_desired_dps(index)
+		print("[%d] %s DPS, %s Desired" % [index, F.t(calc_dps), F.t(desired)])
+
+
+func get_desired_dps(index: int) -> float:
+	return 200 + 200 * index + 30 * 1.25 ** index
