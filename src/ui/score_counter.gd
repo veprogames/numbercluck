accf6d4fe72e08f1d108313655282ddb77cd6fa4
veprogames <75524847+veprogames@users.noreply.github.com>
2025-02-01 19:37:41 +0100

    smooth score counter

--- src/ui/score_counter.gd
+++ src/ui/score_counter.gd
@@ -5,10 +5,20 @@ extends Panel
 
 @export var score: Score
 
+var target_score: float = 0
+var current_score: float = 0
+
 
 func _ready() -> void:
 	score.score_changed.connect(_on_score_changed)
 
 
+func _process(delta: float) -> void:
+	current_score = lerpf(current_score, target_score, 4 * delta)
+	current_score = move_toward(current_score, target_score, target_score * 0.1 * delta)
+	
+	label.text = F.t(current_score)
+
+
 func _on_score_changed(new_score: float) -> void:
-	label.text = F.t(new_score)
+	target_score = new_score
