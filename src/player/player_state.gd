f2feaae51ab341f876b2a5451ce6accfe6d1e61a
veprogames <75524847+veprogames@users.noreply.github.com>
2025-02-03 19:34:53 +0100

    refactor player_state into state machine (enum)

--- src/player/player_state.gd
+++ src/player/player_state.gd
@@ -1,17 +1,31 @@
 class_name PlayerState
 extends Node
 
-signal player_spawned(player: Player)
+enum PlayerStates {
+	NORMAL,
+	BOOSTED,
+	DEAD,
+	INVINCIBLE,
+	GAME_OVER,
+}
+
+# emitted on state changes
+signal player_spawned
 signal player_damaged
+signal boost_started
+signal boost_ended
+signal game_over
+
 signal lives_changed(lives: int)
 signal firepower_changed(fp: int)
-signal game_over
 
 @onready var respawn_timer: Timer = $RespawnTimer
 @onready var viewport_rect: Rect2 = get_viewport().get_visible_rect()
 @onready var level: Node2D = get_tree().get_first_node_in_group(&"level")
 
 var player: Player
+
+var state: PlayerStates = PlayerStates.DEAD
 
 var lives: int = 3 :
 	set(l):
@@ -30,17 +44,15 @@ var max_firepower: int = int(Game.upgrades.max_firepower.get_effect())
 
 var firepower_progress: float = randf()
 
-var mission_completed: bool = false
-
 const PlayerScene: PackedScene = preload("res://src/player/player.tscn")
 const FirepowerScene: PackedScene = preload("res://src/collectables/firepower.tscn")
 
 
 func _ready() -> void:
-	player = respawn_player()
+	set_state(PlayerStates.NORMAL)
 	firepower = min_firepower
 	Events.mission_completed.connect(func() -> void:
-		mission_completed = true
+		set_state(PlayerStates.INVINCIBLE)
 	)
 
 
@@ -71,39 +83,13 @@ func get_normalized_firepower() -> int:
 	return firepower - min_firepower
 
 
-func _on_player_damaged() -> void:
-	if mission_completed:
-		return
-	
-	lives -= 1
-	player_damaged.emit()
-	#@warning_ignore("integer_division")
-	var target_fp: int = min_firepower + int(get_normalized_firepower() / 1.5)
-	firepower = maxi(target_fp, min_firepower)
-	player.damaged.disconnect(_on_player_damaged)
-	player.firepower_collected.disconnect(_on_player_firepower_collected)
-	player.queue_free()
-	if is_dead():
-		game_over.emit()
-	else:
-		respawn_timer.start()
-		await respawn_timer.timeout
-		player = respawn_player()
-
-
-func _on_player_firepower_collected() -> void:
-	if firepower < max_firepower:
-		firepower += 1
-
-
 func respawn_player() -> Player:
 	var player_: Player = PlayerScene.instantiate()
 	player_.position = viewport_rect.position + viewport_rect.size * Vector2(0.5, 0.9)
-	player_.damaged.connect(_on_player_damaged)
+	player_.damaged.connect(_on_player_player_damaged)
 	player_.firepower_collected.connect(_on_player_firepower_collected)
 	add_child(player_)
 	player_.player_firepower.current_power = get_effective_firepower()
-	player_spawned.emit(player_)
 	return player_
 
 
@@ -122,6 +108,39 @@ func spawn_firepower(position: Vector2) -> void:
 	level.add_child(fp)
 
 
+func set_state(new_state: PlayerStates) -> void:
+	match [state, new_state]:
+		[PlayerStates.INVINCIBLE, PlayerStates.DEAD]:
+			return
+		[var s, PlayerStates.DEAD] when s != PlayerStates.DEAD:
+			player_damaged.emit()
+		
+		[PlayerStates.INVINCIBLE, PlayerStates.GAME_OVER]:
+			return
+		[var s, PlayerStates.GAME_OVER] when s != PlayerStates.GAME_OVER:
+			game_over.emit()
+		
+		[var s, PlayerStates.NORMAL] when s == PlayerStates.DEAD:
+			player_spawned.emit()
+		
+		[PlayerStates.BOOSTED, var s] when s != PlayerStates.BOOSTED:
+			boost_ended.emit()
+		[PlayerStates.NORMAL, PlayerStates.BOOSTED], \
+		[PlayerStates.INVINCIBLE, PlayerStates.BOOSTED]:
+			boost_started.emit()
+	
+	state = new_state
+
+
+func _on_player_firepower_collected() -> void:
+	if firepower < max_firepower:
+		firepower += 1
+
+
+func _on_player_player_damaged() -> void:
+	set_state(PlayerStates.DEAD)
+
+
 func _on_game_over() -> void:
 	var title: Title = Title.create("Game Over!")
 	add_child(title)
@@ -132,3 +151,25 @@ func _on_game_over() -> void:
 
 func _on_tree_exited() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+
+func _on_player_spawned() -> void:
+	player = respawn_player()
+
+
+func _on_player_damaged() -> void:
+	lives -= 1
+	
+	var target_fp: int = min_firepower + int(get_normalized_firepower() / 1.5)
+	firepower = maxi(target_fp, min_firepower)
+	
+	player.damaged.disconnect(_on_player_player_damaged)
+	player.firepower_collected.disconnect(_on_player_firepower_collected)
+	player.queue_free()
+	
+	if is_dead():
+		set_state(PlayerStates.GAME_OVER)
+	else:
+		respawn_timer.start()
+		await respawn_timer.timeout
+		set_state(PlayerStates.NORMAL)
