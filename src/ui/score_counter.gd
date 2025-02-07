cd4305444777ede8a107dd1991f3ed798ff742e1
veprogames <75524847+veprogames@users.noreply.github.com>
2025-02-07 13:41:49 +0100

    fix score counter speed

--- src/ui/score_counter.gd
+++ src/ui/score_counter.gd
@@ -8,17 +8,19 @@ extends Panel
 var target_score: float = 0
 var current_score: float = 0
 
+var constant_speed: float = 0
 
 func _ready() -> void:
 	score.score_changed.connect(_on_score_changed)
 
 
 func _process(delta: float) -> void:
-	current_score = lerpf(current_score, target_score, 4 * delta)
-	current_score = move_toward(current_score, target_score, target_score * 0.1 * delta)
+	current_score = lerpf(current_score, target_score, 3 * delta)
+	current_score = move_toward(current_score, target_score, constant_speed * 0.1 * delta)
 	
 	label.text = F.n(current_score)
 
 
 func _on_score_changed(new_score: float) -> void:
+	constant_speed = absf(new_score - target_score)
 	target_score = new_score
