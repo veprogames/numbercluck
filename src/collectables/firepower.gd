f68fbe33089ddc676d1c722ebec2dd0756930e48
veprogames <75524847+veprogames@users.noreply.github.com>
2025-02-15 10:28:12 +0100

    don't play collect sound when firepower leaves screen

--- src/collectables/firepower.gd
+++ src/collectables/firepower.gd
@@ -2,6 +2,9 @@
 class_name Firepower
 extends Area2D
 
+@onready var audio_stream_player_collect: AudioStreamPlayer2D = $AudioStreamPlayerCollect
+@onready var effect_container: Node = get_tree().get_first_node_in_group(&"effect_container")
+
 
 func _physics_process(delta: float) -> void:
 	position.y += 200 * delta
@@ -12,4 +15,7 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 
 
 func _on_area_entered(_area: Area2D) -> void:
+	if effect_container:
+		audio_stream_player_collect.reparent(effect_container)
+		audio_stream_player_collect.play()
 	queue_free()
