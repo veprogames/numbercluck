4bc0db40c52643d6b64e6d37b4d06b42e0888425
veprogames <75524847+veprogames@users.noreply.github.com>
2025-02-19 19:53:31 +0100

    Update and Localize Credits Menu

--- src/credits/credits_menu.gd
+++ src/credits/credits_menu.gd
@@ -9,3 +9,10 @@ func _unhandled_input(_event: InputEvent) -> void:
 
 func _on_button_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/main_menu/main_menu.tscn")
+
+
+func _on_rich_text_label_top_meta_clicked(meta: Variant) -> void:
+	@warning_ignore("unsafe_cast")
+	var url: String = meta as String
+	if url:
+		OS.shell_open(url)
