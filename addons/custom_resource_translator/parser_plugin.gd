9556f4be49291081672023f39196b986db344ab9
veprogames <75524847+veprogames@users.noreply.github.com>
2025-03-04 20:00:33 +0100

    Update to Godot 4.4

--- addons/custom_resource_translator/parser_plugin.gd
+++ addons/custom_resource_translator/parser_plugin.gd
@@ -2,19 +2,26 @@
 class_name CustomResourceTranslationParserPlugin
 extends EditorTranslationParserPlugin
 
-func _parse_file(path: String, msgids: Array[String], msgids_context_plural: Array[Array]) -> void:
+func _parse_file(path: String) -> Array[PackedStringArray]:
 	var res: Resource = load(path)
 	if not res:
-		return
+		return []
+	
+	var result: Array[PackedStringArray] = []
 
 	if res is UpgradeDefinition:
 		var def: UpgradeDefinition = res as UpgradeDefinition
-		msgids.append(def.title)
-		msgids.append(def.description)
+		result.append(
+			PackedStringArray([def.title, def.description])
+		)
 	
 	if res is MissionDefinition:
 		var def: MissionDefinition = res as MissionDefinition
-		msgids.append(def.title)
+		result.append(
+			PackedStringArray([def.title])
+		)
+	
+	return result
 
 
 func _get_recognized_extensions() -> PackedStringArray:
